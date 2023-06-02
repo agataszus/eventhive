0de728ae2f3339cff57cb6589c29e5e3e4634050
agataszus <agataszus@gmail.com>
2023-06-02 17:10:17 +0200

    feat(lint): lint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,23 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["@typescript-eslint", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": "off",
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
