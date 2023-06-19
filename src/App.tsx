34520c052ed94b8741b0e8ae6ae593b949631906
Agata Szustkiewicz <agataszus@gmail.com>
2023-06-19 18:16:48 +0200

    feat(routes): added routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./styles/_global.scss";
 import { LoginPage } from "./pages/auth/login";
 import { CustomQueryClientProvider } from "./components/customQueryClientProvider/CustomQueryClientProvider";
 import { EventsList } from "./components/eventsList/EventsList";
+import { Layout } from "./components/layout/Layout";
+import { RouterProvider } from "./components/routes/Routes";
 
 function App() {
   return (
     <CustomQueryClientProvider>
       <AuthTokenContextProvider>
-        <LoginPage />
-        <EventsList />
+        <RouterProvider />
+        {/* <LoginPage />
+        <EventsList /> */}
       </AuthTokenContextProvider>
     </CustomQueryClientProvider>
   );
