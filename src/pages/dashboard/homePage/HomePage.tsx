cf2cba6d7bb68fc60800c3cca2dd7af25a4ba6eb
Agata Szustkiewicz <agataszus@gmail.com>
2023-06-21 10:23:42 +0200

    feat(HomePage): added dashboard home page

--- src/pages/dashboard/homePage/HomePage.tsx
+++ src/pages/dashboard/homePage/HomePage.tsx
@@ -1,6 +1,26 @@
+import { CategoriesBox } from "../../../components/categoriesBox/CategoriesBox";
+import { HighlightedEventCard } from "../../../components/highlightedEventCard/HighlightedEventCard";
+import { PopularEvents } from "../../../components/popularEvents/PopularEvents";
+import { TopBar } from "../../../components/topBar/TopBar";
+import styles from "./homePage.module.scss";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <TopBar title="Events" />
+      </div>
+      <div>
+        <div className={styles.content}>
+          <HighlightedEventCard />
+        </div>
         <div className={styles.categoriesBox}>
           <CategoriesBox />
         </div>
         <div>
           <PopularEvents />
         </div>
+      </div>
+    </div>
+  );
+};
