a3166171caa73c59d1c29fc59e8ad45ba20d3d57
Agata Szustkiewicz <agataszus@gmail.com>
2023-07-14 12:18:26 +0200

    feat(*): applied remarks from code review

--- src/components/tooltipElement/TooltipElement.tsx
+++ src/components/tooltipElement/TooltipElement.tsx
@@ -1,29 +1,22 @@
 import AccountCircleLineIcon from "remixicon-react/AccountCircleLineIcon";
 import { Text } from "../text/text";
 import styles from "./tooltipElement.module.scss";
-import { Link } from "react-router-dom";
 
 type TooltipLabelProps = {
   Icon: typeof AccountCircleLineIcon;
   text: string;
-  linkTo: string;
-  onClick?: () => void;
+  onClick: () => void;
 };
 
-export const TooltipElement = ({
-  Icon,
-  text,
-  linkTo,
-  onClick,
-}: TooltipLabelProps) => {
+export const TooltipElement = ({ Icon, text, onClick }: TooltipLabelProps) => {
   return (
-    <Link to={linkTo}>
+    <button>
       <li className={styles.option} onClick={onClick}>
         <Icon className={styles.icon} />
         <Text tag="p" variant="action-5">
           {text}
         </Text>
       </li>
-    </Link>
+    </button>
   );
 };
