f82b95b39f9e13ce896d7d53df80e56c4b5649e0
Agata Szustkiewicz <agataszus@gmail.com>
2023-08-03 16:01:09 +0200

    fix(scroll): fixed scroll on iOS mobile

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -4,9 +4,11 @@ export const useScrollLock = (isScrollLocked: boolean) => {
   useEffect(() => {
     if (isScrollLocked) {
       document.body.style.overflowY = "hidden";
+      document.body.style.touchAction = "none";
       return;
     }
 
     document.body.style.overflowY = "scroll";
+    document.body.style.touchAction = "unset";
   }, [isScrollLocked]);
 };
