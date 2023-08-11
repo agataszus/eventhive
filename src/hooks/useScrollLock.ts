8738ffe0f421a937b466af03bd3972ecb5664b3b
Agata Szustkiewicz <agataszus@gmail.com>
2023-08-11 17:56:14 +0200

    fix(useScrollLock): fixed bugs in useScrollLock

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
+import { MOBILE, TABLET, useMediaQueries } from "./useMediaQueries";
 
 export const useScrollLock = (isScrollLocked: boolean) => {
-  useEffect(() => {
-    if (isScrollLocked) {
-      document.body.style.overflowY = "hidden";
-      document.body.style.touchAction = "none";
-      return;
-    }
+  const mediaQuery = useMediaQueries();
 
-    document.body.style.overflowY = "scroll";
-    document.body.style.touchAction = "unset";
-  }, [isScrollLocked]);
+  useEffect(() => {
+    if ([MOBILE, TABLET].includes(mediaQuery)) {
+      if (isScrollLocked) {
+        document.body.style.overflowY = "hidden";
+        document.body.style.touchAction = "none";
+        return;
+      }
+
+      document.body.style.overflowY = "scroll";
+      document.body.style.touchAction = "unset";
+    }
+  }, [isScrollLocked, mediaQuery]);
 };
